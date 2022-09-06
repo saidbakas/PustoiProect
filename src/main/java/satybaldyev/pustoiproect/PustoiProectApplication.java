4fcdbaf2a339508d95cd0f6b8bd093f7233ef4f1
imarov11 <saidibakaskambarbekovich@gmail.com>
2022-09-06 18:33:56 +0300

    Initial commit

---
+++ src/main/java/satybaldyev/pustoiproect/PustoiProectApplication.java
@@ -0,0 +1,13 @@
+package satybaldyev.pustoiproect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PustoiProectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PustoiProectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/satybaldyev/pustoiproect/PustoiProectApplicationTests.java
@@ -0,0 +1,13 @@
+package satybaldyev.pustoiproect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PustoiProectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
